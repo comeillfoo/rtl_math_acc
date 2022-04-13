378a1be9496b85f2e58ff8e195a4be1705f8ba05
come_ill_foo <rayveleaveme34@inbox.ru>
2022-04-13 15:11:11 +0300

    fix: naming

--- rtl_math_acc.srcs/sim_1/new/func_tb.v
+++ rtl_math_acc.srcs/sim_1/new/func_tb.v
@@ -24,15 +24,15 @@ func f(
 
 always #10 clk = ~clk;
 
-reg [7:0] expected_val;
+reg [15:0] expected_val;
 
 integer i, j;
 initial begin
     clk = 1;
-    for ( i = 0; i < 16; i = i + 1 ) begin
-        for ( j = 0; j < 256; j = j + 1 ) begin
-            b = i * i;
-            a = j;
+    for ( i = 0; i < 256; i = i + 1 ) begin
+        for ( j = 0; j < 16; j = j + 1 ) begin
+            b = j * j;
+            a = i;
             rst = 1;
             
             #10
@@ -43,7 +43,7 @@ initial begin
             if ( expected_val == y ) begin
                 $display( "CORRECT: actual: %d, expected: %d", y, expected_val );
             end else begin
-                $display( "ERROR: actual: %d, expected: %d, a: %d, b: %d", y, expected_val, j, i * i );
+                $display( "ERROR: actual: %d, expected: %d, a: %d, b: %d", y, expected_val, i, j * j );
             end
         end
     end
